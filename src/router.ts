5b990bb3cbf33ea36fb9009be0144a8d2d38b711
SofiDev <angiemeow89@gmail.com>
2025-07-05 00:19:34 -0600

    🚧 Avnaces en la clase

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,15 @@
 import {Router} from 'express';
-
+import User from './models/User';
 
 const router = Router();
 
 
 
+
 //! authentication and registration
-router.post('/auth/register', (req, res)=>{
-    console.log(req.body);
+router.post('/auth/register', async(req, res)=>{
+    await User.create(req.body)
+
 })
 
 
