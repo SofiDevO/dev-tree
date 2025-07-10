cd1eac479ba7eb22a8b73aacc8d2534b4debd2dc
SofiDevO <angiemeow89@gmail.com>
2025-07-10 00:24:55 -0600

    🚧 Avances en proyecto

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -3,6 +3,16 @@ import { Request, Response } from "express";
 
 const createAccount = async (req:Request, res:Response) => {
 
+    const {email} = req.body;
+
+    const userExist = await User.findOne({email});
+
+    if(userExist){
+        const error = new Error('User already exist');
+        res.json({error: error.message});
+    }
+    return
+
     const user = new User(req.body);
     await user.save();
     res.json({ message: 'User registered successfully' });
